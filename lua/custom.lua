a7544fe1601c10e2587b55b6d604bcbc7a6cf295
Senthil Arivudainambi <me@sent-hil.com>
2024-10-28 14:21:56 -0700

    add toggleterm-manager to manage terms in telescope

--- lua/custom.lua
+++ lua/custom.lua
@@ -22,8 +22,12 @@ M.plugins = {
     },
   },
   {
-    "akinsho/toggleterm.nvim",
-    version = "*",
+    "ryanmsnyder/toggleterm-manager.nvim",
+    dependencies = {
+      "akinsho/nvim-toggleterm.lua",
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- only needed because it's a dependency of telescope
+    },
     config = true,
   },
   {
