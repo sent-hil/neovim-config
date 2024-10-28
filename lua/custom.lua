d66bce308c107f66a925f3127b4ec5fe3df0f788
Senthil Arivudainambi <me@sent-hil.com>
2024-10-27 23:33:22 -0700

    add multicursors plugin

--- lua/custom.lua
+++ lua/custom.lua
@@ -48,6 +48,30 @@ M.plugins = {
     },
     cmd = "Trouble",
   },
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvimtools/hydra.nvim",
+    },
+    opts = {},
+    cmd = {
+      "MCstart",
+      "MCvisual",
+      "MCclear",
+      "MCpattern",
+      "MCvisualPattern",
+      "MCunderCursor",
+    },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
+  },
 }
 
 -- add extra configuration options here, like extra autocmds etc.
